2a119fbd2376865a91b6ce05e99583dc63430f8a
asmaplansee-arch <asma.plansee@gmail.com>
2025-11-09 22:17:08 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,11 @@
+// config.js
+const CONFIG = {
+    API_URL: 'https://script.google.com/macros/s/AKfycbyD0OQ80NhjINTiptMYrw-8AYMZqLjYHO85HjmIHtWan7Qs-gthx_5PKu6zYhiqTqQgPg/exec',
+    ENDPOINTS: {
+        DASHBOARD: 'dashboard',
+        INVENTORY: 'inventory',
+        ORDERS: 'orders',
+        SHIPPING: 'shipping',
+        CUSTOMERS: 'customers'
+    }
+};
